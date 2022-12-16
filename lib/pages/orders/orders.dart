ba9a04f970b48e0d005a34664007e3f5cf062847
Lucas-Franson <lucasfranson.8@hotmail.com>
2022-12-16 16:53:10 -0300

    Criado componente de pedidos

--- lib/pages/orders/orders.dart
+++ lib/pages/orders/orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/utils/customStyles.dart';
+
+import '../../components/order-item.dart';
 
 class Orders extends StatefulWidget {
   const Orders({Key? key}) : super(key: key);
@@ -7,21 +10,44 @@ class Orders extends StatefulWidget {
   State<Orders> createState() => _OrdersState();
 }
 
+List<OrderItem> orders = [
+    OrderItem(
+        code: 34523, datetime: DateTime.now(), price: 29),
+    OrderItem(
+        code: 34525, datetime: DateTime.now(), price: 45)
+  ];
+
+Widget _buildItem(int index) {
+  OrderItem order = orders[index];
+  return Padding(
+    padding: cardPadding,
+    child: Container(
+        decoration: cardBoxStyle(),
+        child: OrderItem(
+            code: order.code,
+            price: order.price,
+            datetime: order.datetime)),
+  );
+}
+
 class _OrdersState extends State<Orders> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MEU PEDIDOS'),
+        title: const Text('MEUS PEDIDOS'),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ),
-      ),
+      body: Column(children: [
+        Expanded(
+          child: ListView.separated(
+              itemBuilder: (context, index) => _buildItem(index),
+              separatorBuilder: (context, index) => const SizedBox(
+                    height: 10,
+                  ),
+              itemCount: orders.length),
+        )
+      ])
     );
   }
 }
